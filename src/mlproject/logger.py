b008b39d99dec1de24ecab4d80588f4cebfae32b
suhas9 <suhassaligrama99@gmail.com>
2023-12-27 12:39:46 +0530

    logger and exception

---
+++ src/mlproject/logger.py
@@ -0,0 +1,18 @@
+import logging
+import os 
+from datetime import datetime
+
+
+Log_file = f"{datetime.now().strftime('%m-%d-%Y-%H-%M-%S')}.log"
+log_path = os.path.join(os.getcwd,'logs',Log_file)
+
+os.makedirs(log_path,exist_ok=True)
+
+Log_file_path = os.path.join(log_path,Log_file)
+
+logging.basicConfig(
+
+    filename=Log_file_path,
+    format='[%(asctime)s %(lineno)d %(name)s - %(levelname)s - %(message)s] ',
+    level = logging.INFO
+)
---
+++ template.py
@@ -0,0 +1,44 @@
+import os 
+from pathlib import Path
+import logging
+
+package_name = 'mlproject'
+
+list_of_files = [
+
+    'github/workflow/.gitkeep',
+    f"src/{package_name}/__init__.py",
+    f"src/{package_name}/components/__init__.py",
+    f"src/{package_name}/components/data_ingestion.py",
+    f"src/{package_name}/components/data_transformation.py",
+    f"src/{package_name}/components/model_training.py",
+    f"src/{package_name}/pipelines/__init__.py",
+    f"src/{package_name}/pipelines/training_pipeline.py",
+    f"src/{package_name}/pipelines/prediction_pipeline.py",
+    f"src/{package_name}/logger.py",
+    f"src/{package_name}/exception.py",
+    f"src/{package_name}/utils/__init__.py",
+    "notebooks/research.ipynb",
+    "notebooks/data/.gitkeep",
+    "requirements.txt",
+    "setup.py",
+    "init_setup.sh"
+
+]
+
+#here we will create a directory
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir,filename = os.path.split(filepath)
+
+    """ how exist_ok works : if "directory" already exist,os.makedirs()will not raise an error,
+     and it will do nothing. If "my_directory" does not exist it will create it """
+    
+    if filedir != '':
+        os.makedirs(filedir,exist_ok=True)
+    
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath)==0):
+        with open(filepath,'w') as f:
+            pass
+    else:
+        print('file already exist')
