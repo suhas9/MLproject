eee842264eaa94b4ccd6780acd9302f3675df314
suhas9 <suhassaligrama99@gmail.com>
2024-01-03 10:56:49 +0530

    hyperparameter trainging

--- src/mlproject/utils/utils.py
+++ src/mlproject/utils/utils.py
@@ -5,6 +5,7 @@ import pandas as pd
 import pickle
 from src.mlproject.exception import CustomException
 from src.mlproject.logger import logging
+from sklearn.model_selection import GridSearchCV
 
 from sklearn.metrics import r2_score
 
@@ -20,13 +21,20 @@ def save_object(file_path,obj):
         raise CustomException(e,sys)
     
 
-def evaluate_model(X_train,y_train,X_test,y_test,models):
+def evaluate_model(X_train,y_train,X_test,y_test,models,params):
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
-            model.fit(X_train,y_train) #train model
+            para = params[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,para,cv =3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
+            #model.fit(X_train,y_train) #train model
 
             y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
